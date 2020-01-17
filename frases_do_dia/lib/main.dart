09df9e354d654be91804fa1b0fdc162a0f3d1b0f
Eder Almeida <ederdoalmeida@hotmail.com>
2020-01-16 22:28:36 -0300

    Versao 0.1

---
+++ frases_do_dia/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( MaterialApp(
+    title: "Frases do dia",
+    home: Column(),
+    //home: Container(color: Colors.black,),
+
+
+  ));
+
+}
