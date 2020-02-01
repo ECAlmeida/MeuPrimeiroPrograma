c18e1559bcd622f56ed1e14f68d2e2da268ffed4
Eder do Carmo Almeida <ederdoalmeida@hotmail.com>
2020-02-01 17:50:30 -0300

    Calculadora IPVA

---
+++ calculadoraipva/lib/Home.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerprecoveiculo = TextEditingController();
+  String _textoResultado = "";
+
+
+  void _calcular(){
+
+    double precoveiculo = double.tryParse( _controllerprecoveiculo.text );
+
+    if( precoveiculo == null ){
+      setState(() {
+        _textoResultado = "Número inválido, digite números maiores que 0 e utilizando (.) ";
+      });
+    }else{
+      setState(() {
+        precoveiculo= precoveiculo * 0.04;
+        _textoResultado = "Valor do IPVA " + precoveiculo.toStringAsFixed(2) + " reais";
+      });
+
+      //_limparCampos();
+
+    }
+
+
+  }
+
+  void _limparCampos(){
+
+    _controllerprecoveiculo.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("IPVA SP"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                //child: Image.asset("imagens/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual o valor do seu IPVA - SP",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Valor do Veiculo ex: 999999.99"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerprecoveiculo,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    onPressed: _calcular
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
